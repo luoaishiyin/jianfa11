64528f49b1ada72d58ce163278abf91c1cbf2f08
luoai <707531249@qq.com>
2019-04-28 08:20:42 +0800

    xx

--- src/main/java/com/atguigu/jianfa/xixindafa.java
+++ src/main/java/com/atguigu/jianfa/xixindafa.java
@@ -3,5 +3,6 @@ package com.atguigu.jianfa;
 public class xixindafa {
     public static void main(String[] args) {
         System.out.println("xixindafahao");
+        System.out.println("luomeng");
     }
 }
